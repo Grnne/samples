4218946736ee49c36de83e077999ddbb8c20fa1c
Serge Zakhary <grimglance@gmail.com>
2024-10-05 02:43:59 +0700

    remade methods and component

--- js.js
+++ js.js
@@ -2,60 +2,60 @@ Vue.createApp({})
     .component("PhoneBook", {
         data() {
             return {
+                mockPhones: ["1", "2", "3", "4", "5"],
                 myInputs: [
                     {
                         id: 1,
-                        name: "FirstName",
+                        name: "Name",
                         invalidMessage: "This field is required.",
                         validMessage: "Good1.",
-                        value: ''
+                        value: '',
+                        validationMethod: this.validateDefault
                     },
                     {
                         id: 2,
-                        name: "must be>3<10",
+                        name: "Phone",
                         invalidMessage: "This field must be longer than 3 characters.",
                         validMessage: "Good2.",
-                        value: ''
+                        value: '',
+                        validationMethod: this.validatePhone
                     },
                     {
                         id: 3,
                         name: "Dummy1",
                         invalidMessage: "This field is required.",
                         validMessage: "Good3.",
-                        value: ''
+                        value: '',
+                        validationMethod: this.validateDefault
                     },
                     {
                         id: 4,
                         name: "Dummy2",
                         invalidMessage: "This field is required.",
                         validMessage: "Good4.",
-                        value: ''
+                        value: '',
+                        validationMethod: this.validateDefault
                     }
                 ],
                 submitted: false,
-                returnedInputValue: ""
             };
         },
         methods: {
             getInputClass(input) {
                 if (this.submitted) {
-                    return this.validateInput(input) ? 'is-valid' : 'is-invalid';
+                    return input.validationMethod(input) ? 'is-valid' : 'is-invalid';
                 }
-
                 return "";
             },
-            validateInput(input) {
-                if (input.name === "must be>3<10") {
-                    return this.validateCustom(input);
-                }
+            validateDefault(input) {
                 return input.value.trim() !== "";
             },
-            validateCustom(input) {
-                if (input.value.length < 4) {
-                    input.invalidMessage = "Must be longer than 3 characters.";
+            validatePhone(input) {
+                if (this.mockPhones.some(item => item.trim() === input.value.trim())) {
+                    input.invalidMessage = "Phone already exists.";
                     return false;
-                } else if (input.value.length > 10) {
-                    input.invalidMessage = "Must be shorter than 10 characters.";
+                } else if (input.value.length < 7) {
+                    input.invalidMessage = "Must be longer than 7 characters.";
                     return false;
                 }
                 input.invalidMessage = "";
@@ -64,7 +64,7 @@ Vue.createApp({})
             handleSubmit() {
                 this.submitted = true;
 
-                if (this.myInputs.every(input => this.validateInput(input))) {
+                if (this.myInputs.every(input => this.validateDefault(input))) {
                     alert("Form submitted successfully!");
                     this.myInputs.forEach(i => i.value = "");
                     this.submitted = false; // Reset submission state
@@ -76,37 +76,41 @@ Vue.createApp({})
         template: `
           <form @submit.prevent="handleSubmit" class="needs-validation" novalidate>
             <div v-for="(input) in myInputs" :key="input.id">
-              <label :for="input.name">{{ input.name }}</label>
               <my-input
-                  type="text"
-                  :value="input.value"
+                  :input="input"
+                  :validationClass="getInputClass(input)"
                   @update:value="input.value = $event"
-                  :id="input.name"
-                  :class="getInputClass(input)"
-                  class="form-control"
-                  required
               />
-              <div class="valid-feedback">{{ input.validMessage }}</div>
-              <div class="invalid-feedback">{{ input.invalidMessage }}</div>
             </div>
-
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
         `
     })
     .component("myInput", {
         props: {
-            value: {
+            validationClass: {
                 type: String,
                 default: ""
-            }
+            },
+            input: {
+                type: Object,
+                required: true
+            },
         },
-
-        template:
-            `<input
-                :value="value"
+        template: `
+          <div>
+            <label :for="input.name">{{ input.name }}</label>
+            <input
+                :value="input.value"
+                :class="validationClass"
+                class="form-control"
+                type="text"
+                required
                 @input="$emit('update:value', $event.target.value)"
             />
-            `
+            <div class="valid-feedback">{{ input.validMessage }}</div>
+            <div class="invalid-feedback">{{ input.invalidMessage }}</div>
+          </div>
+        `
     })
     .mount("#app");
